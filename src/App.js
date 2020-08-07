fdd145851c5b486d4c256ea0f8bd87249730ee50
Saulo Melo <saulojmc@id.uff.br>
2020-08-07 14:30:16 -0300

    Salvando os textos no local storage.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,66 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import { Form } from "./components/form";
 import List from "./components/list";
 
 export default function App() {
-  const [tasks, setTask] = useState([]);
+  const [tasks, setTask] = useState();
 
   const addTask = (task) => {
     const newTask = {
       task,
       isComplete: false
     };
-    setTask([...tasks, newTask]);
+
+    const newTasks = [...(tasks || []), newTask];
+    setTask(newTasks);
+    saveTasks(newTasks);
   };
 
   const deleteTask = (index) => {
     let newTasks = [...tasks];
     newTasks.splice(index, 1);
     setTask(newTasks);
+    saveTasks(newTasks);
   };
 
   const togleComplete = (index) => {
     const newTasks = [...tasks];
     newTasks[index].isComplete = !newTasks[index].isComplete;
     setTask(newTasks);
+    saveTasks(newTasks);
   };
+
+  const saveTasks = (tasksToSave) => {
+    localStorage.setItem("tasks", JSON.stringify(tasksToSave));
+  };
+
+  const loadTasks = () => {
+    const loadedTasks = JSON.parse(localStorage.getItem("tasks"));
+    return loadedTasks;
+  };
+
+  useEffect(() => {
+    if (tasks) {
+      saveTasks(tasks);
+    }
+  }, [tasks]);
+
+  useEffect(() => {
+    const loadedTasks = loadTasks();
+    setTask(loadedTasks);
+  }, []);
 
   return (
     <div className="App">
       <Form onSave={addTask} />
-      <List
-        tasks={tasks}
-        deleteTask={deleteTask}
-        completeTask={togleComplete}
-      />
+      {tasks ? (
+        <List
+          tasks={tasks}
+          deleteTask={deleteTask}
+          completeTask={togleComplete}
+        />
+      ) : null}
     </div>
   );
 }
--- src/components/list.js
+++ src/components/list.js
@@ -8,26 +8,29 @@ export default function List(props) {
     <div className="tasks-list">
       {tasks.map((task, index) => {
         return (
-          <div className="task-card" key={index}>
-            <span className={task.isComplete ? "complete" : ""}>
-              {task.task}
-            </span>
+          <li key={index}>
+            <div className="task-card">
+              <span className={task.isComplete ? "complete" : ""}>
+                {task.task}
+              </span>
 
-            <div className="action">
-              <Button
-                variant="contained"
-                onClick={() => props.deleteTask(index)}
-              >
-                Excluir
-              </Button>
-              <Button
-                variant="contained"
-                onClick={() => props.completeTask(index)}
-              >
-                Completar
-              </Button>
+              <div className="action">
+                <Button
+                  variant="outlined"
+                  color="secondary"
+                  onClick={() => props.deleteTask(index)}
+                >
+                  Excluir
+                </Button>
+                <Button
+                  variant="outlined"
+                  onClick={() => props.completeTask(index)}
+                >
+                  Completar
+                </Button>
+              </div>
             </div>
-          </div>
+          </li>
         );
       })}
     </div>
